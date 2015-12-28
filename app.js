762fca4d67b6b00421801d8117259300858bf4ff
Jack Tibbs <jdtibbs@users.noreply.github.com>
2015-12-28 07:36:58 -0600

    init

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+app.get('/', function(req, res) {
+	res.send({
+		payload: [{
+			"name": "sam",
+			"role": "admin"
+		}, {
+			"name": "ann",
+			"role": "exec"
+		}, {
+			"name": "dan",
+			"role": "tech"
+		}, {
+			"name": "stan",
+			"role": "admin"
+		}, {
+			"name": "pam",
+			"role": "tech"
+		}, {
+			"name": "spam",
+			"role": "exec"
+		}],
+		stamp: new Date()
+	});
+});
+
+var server = app.listen(process.env.PORT || 80, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('App is listening at http://%s:%s', host, port);
+});
